1ed09a8a60cdbba0883f266aa0f26ccd3f2c3d4e
alejandrosoriaa-tappt <alejandro.soria.a@gmail.com>
2026-08-12 14:36:31 -0600

    feat(docquad): crea detector aislado con runtime intercambiable

---
+++ scanner/docquad/detector.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const fs = require('fs');
+const { asegurarModelo } = require('./model');
+const { prepararEntrada } = require('./preprocess');
+const { postprocesarEsquinas } = require('./postprocess');
+const { OrtRuntimeNode } = require('./ort-runtime.node');
+
+class DocQuadDetector {
+  constructor({ runtime = null, modelPath = null } = {}) {
+    this.runtime = runtime;
+    this.modelPath = modelPath;
+  }
+
+  async init() {
+    if (!this.modelPath) this.modelPath = await asegurarModelo();
+    if (!this.runtime) this.runtime = new OrtRuntimeNode(this.modelPath);
+    await this.runtime.init();
+    return this;
+  }
+
+  async detectarBuffer(buffer) {
+    if (!Buffer.isBuffer(buffer)) throw new Error('docquad_input_not_buffer');
+    if (!this.runtime) await this.init();
+
+    const inicio = Date.now();
+    const preparado = await prepararEntrada(buffer);
+    const inferenciaInicio = Date.now();
+    const outputs = await this.runtime.run(preparado.input);
+    const inferenciaMs = Date.now() - inferenciaInicio;
+
+    const post = postprocesarEsquinas(
+      outputs.cornerHeatmaps,
+      preparado.letterbox,
+      preparado.srcW,
+      preparado.srcH
+    );
+
+    return {
+      ...post,
+      source: 'docquad',
+      image: { width: preparado.srcW, height: preparado.srcH },
+      timing: {
+        inferenceMs: inferenciaMs,
+        totalMs: Date.now() - inicio,
+      },
+      outputs: {
+        maskShape: outputs.maskLogits.dims,
+        cornerShape: outputs.cornerHeatmaps.dims,
+      },
+    };
+  }
+
+  async detectarArchivo(filePath) {
+    return this.detectarBuffer(fs.readFileSync(filePath));
+  }
+}
+
+module.exports = { DocQuadDetector };
